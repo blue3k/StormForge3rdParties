1c138f36c7b5e8c08b9831e13aec75eeeec7f819
blue3k <blue3k@gmail.com>
2019-12-21 13:03:26 -0800

    zookeeper: fix for android compile

--- 3rdParties/src/zookeeper/zookeeperConnector/src/mt_adaptor.c
+++ 3rdParties/src/zookeeper/zookeeperConnector/src/mt_adaptor.c
@@ -483,13 +483,14 @@ int32_t inc_ref_counter(zhandle_t* zh,int i)
 int32_t fetch_and_add(volatile int32_t* operand, int incr)
 {
 #ifndef WIN32
-    int32_t result;
-    asm __volatile__(
-         "lock xaddl %0,%1\n"
-         : "=r"(result), "=m"(*(int *)operand)
-         : "0"(incr)
-         : "memory");
-   return result;
+    //int32_t result;
+    //asm __volatile__(
+    //     "lock xaddl %0,%1\n"
+    //     : "=r"(result), "=m"(*(int *)operand)
+    //     : "0"(incr)
+    //     : "memory");
+   //return result;
+   return __sync_fetch_and_add(operand, incr);
 #else
 	return InterlockedAdd(operand, incr);
 #endif
